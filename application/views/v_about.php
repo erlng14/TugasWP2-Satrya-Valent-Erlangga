2c2e53134d1af3ed703c93794abced04edfa0611
erlng14 <valenterlangga14@gmail.com>
2023-04-16 22:55:34 +0700

    mengumpulkan about

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+    <li>Nama Lengkap : Satrya Valent Erlangga</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+    <li> Jl. Raden Saleh, RT.001/RW.006, Karang Mulya, Kec. Karang Tengah, Kota Tangerang, Banten 15157</li>
+ </ul>
+ 
+ <h4>Tempat Tanggal Lahir</h4>
+ <ul type="none">
+    <li>Jakarta, 14 Februari 2001</li>
+ </ul>
+ <h4>Hobi</h4>
+ <ul type="square">
+    <li>Gapunya</li>
+ </ul>
+</section>
